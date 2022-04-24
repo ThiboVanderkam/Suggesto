4624845753c26cad86e21ee50ffb30548c25acd1
Cedrico03 <cedric.van.riet@gmail.com>
2022-04-24 13:32:39 +0200

    scraper aangepast + writen to txt file v categorien

--- v1/assets/python/scraperBol2.py
+++ v1/assets/python/scraperBol2.py
@@ -1,4 +1,3 @@
-from pydoc_data.topics import topics
 import requests
 from bs4 import BeautifulSoup
 
@@ -24,6 +23,17 @@ def CreateLink():
     deLink = begin + inbut + eind
     return(deLink)
 
+def MakeLink(product):  #dit is als alternatief als je geen input wilt
+
+    product = product.replace(" ", "+")
+
+    begin = "https://www.bol.com/be/nl/s/?page=1&searchtext="
+    eind = "&view=list&sort=popularity1"
+
+    deLink = begin + product + eind
+    return(deLink)
+
+
 
 def GetThem(link, hoeveelheid):
     # linkBolHome = "https://www.bol.com/be/nl/s/?page=1&searchtext=voor+hem&view=list&sort=popularity1"
@@ -34,6 +44,7 @@ def GetThem(link, hoeveelheid):
     images = []
     prices = []
     ratings = []
+
 
     for i in range(hoeveelheid):
 
@@ -70,13 +81,31 @@ def GetThem(link, hoeveelheid):
     # print(images)
     # print(prices)
     # print(ratings)
+    
+    # -------------------------------WRITING TO FILE-------------------------------
+    # EERST CONTENT V FILE VERWIJDEREN
+    # ZIE LIJN 132 => hierdoor w de contents verwijderd
 
-    for i in range(hoeveelheid):
+    # SCHRIJF DE ITEMS NAAR FILE
+
+    file = open("itemsCategorie.txt", "a")
+
+    for i in range(hoeveelheid):                                                                        # bij de link moet erachter een spatie (anders w prijs meegenomen in link)
+        file.write(str(topItems[i]).replace("é", "e").replace("è", "e").replace("ö", "o").replace('®', "").replace("™", "") + ";" + str(links[i])  + " " + ";" + str(images[i]) + " " + ";" + str(prices[i]).replace('€', "euro: ") + ";" + str(ratings[i]) + "\n")
+
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
         print(str(topItems[i]) + " - " + str(links[i]) + " - " + str(images[i]) + " - " + str(prices[i]) + " - " + str(ratings[i]))
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
+    file.write("\n") # makkelijk categorien v elkaar te onderscheiden
+    file.close()
 # print(topItems + " - " + links + " - " + images + " - " + prices + " - " + ratings)
+
+def GetCategories(lijstMetCategorien):
+    for categorie in lijstMetCategorien:
+        link = MakeLink(categorie)
+        GetThem(link, 10)
+
 
 #_______________________________________________________________________________________________________ EINDE VAN DE FUNCTIES
 
@@ -93,11 +122,26 @@ LinkBolVoorHaar = "https://www.bol.com/be/nl/s/?page=1&searchtext=voor+haar&view
 
 # linkos = "https://www.bol.com/be/nl/s/?page=1&searchtext=fortnite&view=list&sort=popularity1"
 # GetThem(linkos, 4)
-
-linkos = CreateLink()
 # linkos = "https://www.bol.com/be/nl/s/?searchtext=voor+hem"
-GetThem(linkos, 7)
-print(linkos)
+
+# linkos = CreateLink()
+# GetThem(linkos, 10)
+# print(linkos)
+
+# -------------- TEST V FUNCTIE getCategories --------------
+open("itemsCategorie.txt", "w").close() # file leegmaken
+
+lijstMetCategorien = ["video games", "boeken", "nature", "photo", "sports", "tech", "beauty"]
+GetCategories(lijstMetCategorien)
+
+
 
 #     https://www.bol.com/be/nl/s/?searchtext=voor+hem    op relevantie
 #     https://www.bol.com/be/nl/s/?page=1&searchtext=voor+hem&view=list&sort=popularity1    op populariteit
+# video games (games werkt niet lijn 42)
+# boeken
+# nature
+# photo (photography werkt niet)
+# sports
+# tech
+# beauty
