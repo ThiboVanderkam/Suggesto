6a923b87715fe6b3ab168e3b1eab2518a8e98b7e
Robert Banea <robertbanea113@gmail.com>
2022-05-01 22:22:57 +0200

    added people to calendar

--- v1/assets/js/calendar.js
+++ v1/assets/js/calendar.js
@@ -1,5 +1,22 @@
-console.log("loaded")
-dateElements = document.querySelectorAll("li");
+dateElements = Array.from(document.querySelector(".dates").children);
 
-console.log(dateElements)
+bdayData = {
+    "Jan Janssens1": "2022-05-06",
+    "Jan Janssens1": "2022-06-06",
+    "abc": "2022-05-06",
+    "abc": "2022-05-06",
+    "Jan Janssens1é": "2022-05-15",
+    "abc3": "2022-05-15",
+    "abc31": "2022-05-15",
+    "abc32": "2022-06-15",
+    "ABC32": "2022-07-15",
+};
+
+for (var i = 0; i < dateElements.length; i++){
+    for (var friend in bdayData){
+        if (dateElements[i].id.toString() == ("li-" + bdayData[friend]).toString()){
+            dateElements[i].innerHTML+= '<br>' + friend;
+        };
+    };
+};
 
